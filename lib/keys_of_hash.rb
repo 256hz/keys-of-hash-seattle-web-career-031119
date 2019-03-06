a961c7f16b3edceb20d00cfdc65f09f24cf37040
Abe D <iaryiou111@gmail.com>
2019-03-06 22:27:39 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Hash
   def keys_of(arguments)
     return_array = []
-    binding.pry
+    #binding.pry
     Hash.each do |e|
       return_array >> e if arguments.include?(Hash[e])
     end
