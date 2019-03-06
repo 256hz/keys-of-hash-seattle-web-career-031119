abb209b2bfdd02dcaf9088f65e3fe63e462d1298
Abe D <iaryiou111@gmail.com>
2019-03-06 22:33:19 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,8 +4,8 @@ class Hash
   def keys_of(arguments)
     return_array = []
     #binding.pry
-    Object.each do |e|
-      return_array >> e if arguments.include?(Object[e])
+    self.each do |e|
+      return_array >> e if arguments.include?(self[e])
     end
     return_array
   end
